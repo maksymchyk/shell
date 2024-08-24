303ea54810dc9ad3bf5318e8de274eb682d483f5
Max Parkhomchuk <max@Maxs-MacBook.local>
2024-08-24 18:20:22 -0400

    final changes, errors fixed

--- 02_activities/assignments/assignment.sh
+++ 02_activities/assignments/assignment.sh
@@ -14,9 +14,9 @@ mkdir dir1 dir2 dir3 dir4 dir5
 ls
 
 # 3. Create 5 text files in dir2 named file1.txt, file2.txt, file3.txt, file4.txt, and file5.txt
-touch file1.txt file2.txt file3.txt file4.txt /dir2
+touch dir2/file1.txt dir2/file2.txt dir2/file3.txt dir2/file4.txt
 # 4. Append the words "Hello world" to dir2/file3.txt
-echo "Hello world">dir2/file3.txt
+echo "Hello world" >> dir2/file3.txt
 # 5. Verify that file3.txt contains the words "hello world" by printing the contents of the file in the terminal
 cat dir2/file3.txt
 # 6. Delete file4.txt
